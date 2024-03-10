5e10a810dc9af25fa4ec879d9427f9332207a98d
kunalmore06 <kunalmore112002@gmail.com>
2024-03-11 01:18:39 +0530

    Ready to use

--- springboot-restful-webservices/src/main/java/net/javaguides/springbootrestfulwebservices/entity/Scan.java
+++ springboot-restful-webservices/src/main/java/net/javaguides/springbootrestfulwebservices/entity/Scan.java
@@ -1,11 +1,15 @@
 package net.javaguides.springbootrestfulwebservices.entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import jakarta.persistence.Entity;
 
+
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
@@ -13,6 +17,7 @@ import lombok.Setter;
 public class Scan {
 
         @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Id
         private Long id;
         private String datastoreId;
         // Add other relevant fields
